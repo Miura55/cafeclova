75045b19d7c1bf09493ef66156f781b3d5adc685
Koki Miura <memjoalty@yahoo.co.jp>
2019-12-14 22:26:19 +0900

    First commit

---
+++ action/cafe-clova.py
@@ -0,0 +1,29 @@
+import json
+import cek
+
+def main(args):
+    print(args)
+    clova = cek.Clova(
+        application_id="net.clova.cafe",
+        default_language="ja",
+        debug_mode=True)
+
+    try:
+        if args["request"]["type"] == "LaunchRequest":
+            open_message = "いらっしゃいませ。ご注文をどうぞ。"
+            welcome_message = cek.Message(message=open_message, language="ja")
+            response = clova.response([welcome_message])
+            return response
+        elif args["request"]["type"] == "SessionEndedRequest" :
+            thanks_text = "ありがとうございました。またのご利用をお待ちしております。"
+            bye_message = cek.Message(message=thanks_text, language="ja")
+            response = clova.response([bye_message])
+            return response
+        elif args["request"]["type"] == "IntentRequest":
+            rep_message = "かしこまりました。他にご注文はありますか？"
+            reply_speak = cek.Message(message=rep_message, language="ja")
+            response = clova.response([reply_speak])
+            return response
+    except Exception as e:
+        print(e)
+        return {"status":500}
