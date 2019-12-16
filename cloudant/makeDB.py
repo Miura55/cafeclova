ed56dd4af0957773e49ca8e0eba75678e0988d56
Koki Miura <memjoalty@yahoo.co.jp>
2019-12-17 02:29:56 +0900

    Fix dotenv ¥

--- cloudant/makeDB.py
+++ cloudant/makeDB.py
@@ -2,18 +2,16 @@ import requests
 from dotenv import load_dotenv
 import os
 
-params = (
-    ('blocking', 'true'),
-)
+load_dotenv()
 
 body = {
     "userId":"431",
     "menu":"マカロン",
-    "value":4
+    "value":5
 }
 
 response = requests.post(
-            os.environ("URL")+"/order",
+            os.environ["URL"]+"/order",
             json = body
         )
 print(response.text)
